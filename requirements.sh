a9773e11c17e85b53446a7eced1fcf33c7d03d7d
G.G.S <82466341+Zer0plusOne@users.noreply.github.com>
2024-11-29 16:01:34 +0100

    Update for v_1.0.0
    Added the dependencies for v_1.0.0

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip install requests socket ssl sys nmap whois datetime beautifulsoup4 urllib
+pip install requests socket ssl sys nmap whois datetime beautifulsoup4 urllib wappalyzer-python requests-html
